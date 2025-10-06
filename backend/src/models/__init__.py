1ae38dc2b8a0d1fed33b009911663b96b7e3e51b
alevieirasp <131933806+alevieirasp@users.noreply.github.com>
2025-10-06 00:15:17 -0300

    Update __init__.py

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,2 +1,8 @@
 # Models package
+from flask_sqlalchemy import SQLAlchemy
+
+# objeto global do SQLAlchemy exportado pelo pacote "models"
+db = SQLAlchemy()
+
+__all__ = ["db"]  # opcional, deixa explícito
 
