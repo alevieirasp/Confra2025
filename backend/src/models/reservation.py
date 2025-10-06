3ff2ce65a4becae6a13461ccf4d9c7e5c7c8a7cb
alevieirasp <131933806+alevieirasp@users.noreply.github.com>
2025-10-06 00:22:06 -0300

    Update reservation.py

--- backend/src/models/reservation.py
+++ backend/src/models/reservation.py
@@ -1,6 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy
+from . import db
+from datetime import datetime
 
-# Objeto global do SQLAlchemy exportado pelo pacote "models"
-db = SQLAlchemy()
+class Reservation(db.Model):
+    __tablename__ = 'reservations'
 
-__all__ = ["db"]  # opcional
+    id = db.Column(db.Integer, primary_key=True)
+    bike_id = db.Column(db.Integer, nullable=False, unique=True)
+    name = db.Column(db.String(100), nullable=False)
+    shirt_size = db.Column(db.String(10), nullable=False)
+    shirt_name = db.Column(db.String(100), nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "bike_id": self.bike_id,
+            "name": self.name,
+            "shirt_size": self.shirt_size,
+            "shirt_name": self.shirt_name,
+            "timestamp": self.timestamp.strftime("%d/%m/%Y, %H:%M:%S"),
+        }
