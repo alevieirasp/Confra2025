21d33db137eb1f59571c319b6853298185c96e0f
alevieirasp <131933806+alevieirasp@users.noreply.github.com>
2025-10-06 23:58:07 -0300

    Delete backend directory

--- backend/src/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Models package
-from flask_sqlalchemy import SQLAlchemy
-
-# objeto global do SQLAlchemy exportado pelo pacote "models"
-db = SQLAlchemy()
-
-__all__ = ["db"]  # opcional, deixa explícito
-
